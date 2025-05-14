71367b35ec0c37a95d9de10a1c9457a062c0239e
MinhQuan124 <dminhquan2003@gmail.com>
2025-05-14 23:15:32 +0700

    Update baseURL

--- src/services/apis/axiosClient.js
+++ src/services/apis/axiosClient.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosClient = axios.create({
-  baseURL: "http://localhost:8000/api/v1",
+  baseURL: "https://twodayoutfit-api.onrender.com/api/v1",
   timeout: 10000,
   headers: {
     "Content-Type": "application/json",
